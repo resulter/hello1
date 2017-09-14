601aebaccca3343354cdb0fa0537be3a90815c3d
“foreach <772173722@qq.com>
2017-09-14 09:25:34 +0800

    第一次测试

---
+++ src/MainTest.java
@@ -0,0 +1,25 @@
+import java.io.File;
+public class MainTest {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        File f = new File("E:\\workspace1783\\bpm\\src\\main\\java\\cn\\xdf\\bpm\\bj\\modules\\studyservice\\web");
+        boolean b = false;
+        try {
+            fileList(f);
+        } catch (Exception e) {
+        }
+    }
+    public static void fileList(File file) {
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (File f1 : files) {
+                if(!f1.isDirectory()) {
+                    System.out.println(f1.getAbsolutePath());
+                }
+                fileList(f1);
+            }
+        }
+    }
+}
